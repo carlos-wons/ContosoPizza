131c0653784402197e1e17e94bf84ef0f3c8e24d
Carlos Alberto Contreras ventura <72568840+carlos-wons@users.noreply.github.com>
2022-02-12 14:11:36 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using ContosoPizza.Services;
+using ContosoPizza.Data;
+
+// Additional using declarations
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add the PizzaContext
+
+builder.Services.AddSqlServer<PizzaContext>("Data Source=ContosoPizza.db");
+
+// Add the PromotionsContext
+
+builder.Services.AddScoped<PizzaService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Add the CreateDbInNotExists method call
+//app.CreateDbIfNotExists();
+
+app.Run();
